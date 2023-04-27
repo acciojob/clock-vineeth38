8ee12e9969c72ead73689faf5a0ef156e402b1dc
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-27 19:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var today = new Date();
+  var day = today.getDay();
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
+   
+  document.getElementById("timer").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+ 
+  </script>
+</html>
+
+   
