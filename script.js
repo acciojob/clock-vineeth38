fe1c015ec7a63151a78a775d95f0eea1e6d05d4f
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 20:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //your JS code here. If required.
 function getTime(){
 	const time = new Date();
-	let dayString = '${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}';
+	let dayString = `${time.getMonth()+1}/${time.getDate()}/${time.getFullYear()}`;
 	let hr = time.getHours();
 	let timeString="";
 	if(hr>=12){
 		if(hr===12){
-			timeString='${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}PM'
+			timeString=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} PM`
 		}
 		else{
 			h-=12;
-			timeString='${hr}:${time.getMinutes()}:${time.getSeconds()}PM'
+			timeString=`${hr}:${time.getMinutes()}:${time.getSeconds()} PM`
 		}
 	}
 	else{
-		timeString='${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}AM'
+		timeString=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} AM`
 	}
 	return dayString+","+timeString;
 }
-const p = document.getElementBYId("timer");
+const p = document.getElementById("timer");
 function timer() {
 	p.innerText = getTime();
 }
